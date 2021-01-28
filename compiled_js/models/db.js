3e11b5e4eea0061f211fc96c63ddb770ea8c95e6
Tejas Joshi <tejasjoshi689@gmail.com>
2021-01-28 16:12:09 +0530

    Delete compiled_js directory

--- compiled_js/models/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import {initMysqlConn} from '../models/mysql-utilts'
-// export class Db {
-//     // static campaign:Campaign;
-//     static async init() {
-//         const dbConn = await initMysqlConn(config.serviceMysql,'./data/schema.sql')
-//     }
-// }
